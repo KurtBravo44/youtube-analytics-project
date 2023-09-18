f6ef94150d4947194f8f6386caec12a05bb1bb2b
Kurt Bravo <kurtbravo2344@gmail.ru>
2023-09-18 17:46:03 +0300

    Complete dz_3

--- src/channel.py
+++ src/channel.py
@@ -1,7 +1,12 @@
 import os
+import json
 from googleapiclient.discovery import build
 import isodate
-import json
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
 api_key: str = os.getenv('YT_API_KEY')
 youtube = build('youtube', 'v3', developerKey=api_key)
 
@@ -20,6 +25,28 @@ class Channel:
         self.subscribersCount = self.channel['items'][0]['statistics']['subscriberCount']
         self.video_count = self.channel['items'][0]['statistics']['videoCount']
         self.view_count = self.channel['items'][0]['statistics']['viewCount']
+
+    def __str__(self):
+        return f'{self.title} ({self.url})'
+
+    def __add__(self, other):
+        return int(self.subscribersCount) + int(other.subscribersCount)
+
+    def __sub__(self, other):
+        return int(self.subscribersCount) - int(other.subscribersCount)
+
+    def __gt__(self, other):
+        return int(self.subscribersCount) > int(other.subscribersCount)
+
+    def __ge__(self, other):
+        return int(self.subscribersCount) >= int(other.subscribersCount)
+
+    def __lt__(self, other):
+        return int(self.subscribersCount) < int(other.subscribersCount)
+
+    def __le__(self, other):
+        return int(self.subscribersCount) <= int(other.subscribersCount)
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
