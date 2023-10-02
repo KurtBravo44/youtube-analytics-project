3e805e9da180239ecd43e8ddd5d9b8587699db9e
Kurt Bravo <kurtbravo2344@gmail.ru>
2023-10-02 20:03:19 +0300

    Complete dz_6

--- src/video.py
+++ src/video.py
@@ -3,16 +3,23 @@ from src.channel import youtube
 class Video():
     def __init__(self, video_id):
         self.video_id = video_id
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=video_id
-                                               ).execute()
-        self.title: str = video_response['items'][0]['snippet']['title']
-        self.url = f'https://www.youtube.com/watch?v={video_id}'
-        self.view_count: int = video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = video_response['items'][0]['statistics']['likeCount']
-
+        try:
+            video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                   id=video_id
+                                                   ).execute()
+            self.title: str = video_response['items'][0]['snippet']['title']
+            self.url = f'https://www.youtube.com/watch?v={video_id}'
+            self.view_count: int = video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+        except IndexError:
+            print('Error: Video_id is not exist')
+            self.title = None
+            self.url = None
+            self.view_count= None
+            self.like_count = None
     def __str__(self):
         return self.title
+
 
 class PLVideo(Video):
     def __init__(self, video_id, playlist_id):
